a9105118a62c9c51438ff068af840be8a13bc58c
Andrey Popp <8mayday@gmail.com>
2012-10-30 14:50:21 +0400

    add url to package metadata

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import setup
 
 version = "0.4.8"
 
+with open('README') as f:
+    long_description = f.read()
+
 setup(
     name="configure",
     version=version,
     description="configuration toolkit based on YAML",
+    long_description=long_description,
     author="Andrey Popp",
     author_email="8mayday@gmail.com",
+    url='https://github.com/andreypopp/configure',
     py_modules=["configure"],
     test_suite="tests",
     install_requires=["pyyaml"],
